d740aa6cb2d5edde19449177e26c28a2df2218ac
Quân Nguyễn <quanlh3052004@gmail.com>
2025-07-25 02:31:35 +0100

    [Task] [TK-5] Connect database
    Connect server to mongodb atlas using mongodb library

---
+++ madala/src/models/User.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+
+export interface IUser extends Document {
+  name?: string;
+  email?: string;
+  password?: string;
+  image?: string;
+}
+
+const UserSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// Xuất model
+// tránh lỗi biên dịch lại model đã có trong môi trường dev (do Hot Reload).
+const User = mongoose.models.User || mongoose.model<IUser>('User', UserSchema);
+
+export default User;
